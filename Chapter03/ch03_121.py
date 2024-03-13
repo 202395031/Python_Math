b3a6c7ba9fcdef04b9991c11fe51f9291580db81
천승용 <127080149+202395031@users.noreply.github.com>
2024-03-13 15:08:58 +0900

    Add files via upload

---
+++ Chapter03/ch03_121.py
@@ -0,0 +1,32 @@
+'''
+작성일 : 2024년 03월 13일
+학과 : 컴퓨터공학부
+학번 : 202395031
+이름 : 천승용
+'''
+print('인공지능 수학')
+print()
+
+# 산술 연산자를 이용한 다양한 연산
+a = 7
+b = 4
+print("덧셈 :", a+b)
+print("뺄셈 :", a-b)
+print("곱셈 :", a*b)
+print("나눗셈 :", a/b)
+print("나눗셈(소수점 없음) :", a//b)
+print("나머지 :", a%b)
+print("거듭제곱 :", a**b)
+
+print()
+
+# 제곱근, 지수함수, 로그함수, 삼각함수
+
+import math
+
+print("3의 제곱근 =",math.sqrt(3))
+print("10의 지수함수 =",math.exp(10))
+print("5의 자연로그값 =",math.log(5))
+print("파이의 코사인값 =",math.cos(math.pi * 2))
+print("파이의 사인값 =",math.sin(math.pi * 3 / 2))
+print("파이의 탄젠트값 =",math.tan(math.pi / -4))
